536749ea2626c022c77c0e1af5ca4c611cb1b007
Vishwas Beede <vishwasbeede@gmail.com>
2021-03-10 19:38:42 +0530

    First initialize

--- Space template_files/test.py
+++ Space template_files/test.py
@@ -22,7 +22,7 @@
 # packed=list(zip(colors_lis,ani_lis1))
 
 # colors=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
-date=["18-01-21","28-03-21","12-02-21","11-02-21","21-01-21","28-02-21","06-02-21","18-02-21"]
+date=["18-01-21","28-03-21","12-12-21","11-02-21","21-01-21","28-02-21","06-02-21","18-02-21"]
 amount=["210 D","232 D","278 D","285 D","296 D","150 D","200 C","1300 D","2000 D"]
 info=["Birthaday","Outing","Grocesory","Drinks","Mobile","Data recharge","Dominos","PizzHt"]
 packed=list(zip(date,info,amount))
