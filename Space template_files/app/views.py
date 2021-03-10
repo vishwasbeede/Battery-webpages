b3c92783beaef061ca3fe88e44615f7c32d3ea6c
vishwas bg <vishwasbeede@gmail.com>
2021-03-10 18:47:39 +0530

    First changes

--- Space template_files/app/views.py
+++ Space template_files/app/views.py
@@ -1,4 +1,24 @@
-from app import app
+from app import app,render_template,request,redirect,url_for
+from test import *
+
 @app.route("/")
 def root_fun():
-    return "Hello world"
+
+    return render_template('public/templates/index.html')
+
+
+@app.route("/new.html")
+def return_fun():
+    return render_template("new.html",names="Ganesh")
+
+@app.route("/new.html")
+def return_fun1_1_1():
+    return render_template("new.html",names="Ganesh")
+
+@app.route("/new.html")
+def return_fun2_1():
+    return render_template("new.html",names="Ganesh")
+
+@app.route("/animals")
+def return_fun2():
+    return render_template("aniamls.html",z=packed)
